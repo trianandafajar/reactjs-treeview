0c736100fed35dd4e75d5a238222a892d2535127
trianandafajar <mail.trianandafajar@gmail.com>
2025-05-11 22:41:33 +0700

    Update Home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,31 +1,23 @@
 import Sidebar from "./Sidebar.jsx";
 import PropTypes from "prop-types";
 
-const Home = ({ page }) => {
-  return (
-    <div className="main">
-      <Sidebar />
-      <div className="main-content">
-        <h1 className="title">My {page}</h1>
-        <p className="info">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Consectetur, quasi.
-        </p>
-        <button className="btn" aria-label="See More Information">
-          See More...
-        </button>
-      </div>
+const Home = ({ page = "Page" }) => (
+  <div className="main">
+    <Sidebar />
+    <div className="main-content">
+      <h1 className="title">My {page}</h1>
+      <p className="info">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur, quasi.
+      </p>
+      <button className="btn" aria-label="See More Information">
+        See More...
+      </button>
     </div>
-  );
-};
+  </div>
+);
 
 Home.propTypes = {
   page: PropTypes.string,
-};
-
-// Menambahkan nilai default jika `page` tidak diberikan
-Home.defaultProps = {
-  page: "Page",
 };
 
 export default Home;
